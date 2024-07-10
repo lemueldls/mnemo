a4ae796565a4f1e1e57d0ea85a8cfb4d20eec398
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2024-07-09 20:59:24 -0400

    feat: prepare for android

---
+++ backend/tauri/gen/android/app/src/main/java/com/lemueldls/mnemo/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.lemueldls.mnemo
+
+class MainActivity : TauriActivity()
--- flake.nix
+++ flake.nix
@@ -1,15 +1,33 @@
 {
-  inputs = { utils.url = "github:numtide/flake-utils"; };
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    android = { url = "github:tadfisher/android-nixpkgs"; };
+  };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, android }:
     utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+
+          # android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+          #   cmdline-tools-latest
+          #   build-tools-34-0-0
+          #   platform-tools
+          #   platforms-android-34
+          #   ndk-23-0-7599858
+          # ]);
         in
         {
+          overlay = final: prev: {
+            inherit (self.packages.${final.system}) android-sdk;
+          };
+
           devShell = pkgs.mkShell {
             buildInputs = with pkgs; [
+              # Tauri
+
+              ## Desktop
               pkg-config
               openssl #.out
               glib #.out
@@ -20,9 +38,23 @@
               webkitgtk_4_1
               cairo
               glib-networking
+
+              # ## Android
+              # android-sdk
             ];
 
+            # OpenSSL
+            OPENSSL_DIR = "${pkgs.openssl.dev}";
+            OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+            OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+
+            # Network
             GIO_MODULE_DIR = "${pkgs.glib-networking}/lib/gio/modules";
+
+            # # Android
+            # ANDROID_HOME = "${android-sdk}/share/android-sdk";
+            # ANDROID_SDK_ROOT = "${android-sdk}/share/android-sdk";
+            # NDK_HOME = "${android-sdk}/share/android-sdk/ndk/23.0.7599858";
           };
         }
       );
