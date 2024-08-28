c070d45df7299c7730e704e71ad0f14c96c8b3f8
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2024-08-28 19:58:45 -0400

    feat: api plugin

---
+++ app/plugins/api.ts
@@ -0,0 +1,39 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  // const { session } = useUserSession();
+
+  const runtimeConfig = useRuntimeConfig();
+  const { apiBaseUrl } = runtimeConfig.public;
+
+  if (!apiBaseUrl)
+    throw createError({ message: "NUXT_PUBLIC_API_BASE_URL is not set" });
+
+  const api = $fetch.create({
+    // baseURL: apiBaseUrl,
+    // baseURL: "https://mnemo.nuxt.dev",
+    headers: useRequestHeaders(["cookie"]),
+    // onRequest({ request, options, error }) {
+    //   if (session.value?.token) {
+    //     const headers = (options.headers ||= {});
+    //     if (Array.isArray(headers)) {
+    //       headers.push(["Authorization", `Bearer ${session.value?.token}`]);
+    //     } else if (headers instanceof Headers) {
+    //       headers.set("Authorization", `Bearer ${session.value?.token}`);
+    //     } else {
+    //       headers.Authorization = `Bearer ${session.value?.token}`;
+    //     }
+    //   }
+    // },
+    // async onResponseError({ response }) {
+    //   if (response.status === 401) {
+    //     await nuxtApp.runWithContext(() => navigateTo("/login"));
+    //   }
+    // },
+  });
+
+  // Expose to useNuxtApp().$api
+  return {
+    provide: {
+      api,
+    },
+  };
+});
