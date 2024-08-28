b2bd44bacef619cadd063c37a914223328969b3f
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2024-08-28 18:12:57 -0400

    fix: no-op server composable

--- app/composables/spaces/server.ts
+++ app/composables/spaces/server.ts
@@ -11,7 +11,6 @@
 // import { ref as dbRef, set } from "firebase/database";
 
 import { createClient } from "@supabase/supabase-js";
-import { ElectricDatabase, electrify } from "electric-sql/wa-sqlite";
 // import { schema } from "./generated/client";
 
 // // Initiate your Electric database
@@ -25,39 +24,39 @@ import type { Space } from ".";
 const store = new Map();
 const spaces = shallowRef<{ [key: string]: Space }>({});
 
-if (import.meta.client) {
-  await store.clear();
+// if (import.meta.client) {
+//   await store.clear();
 
-  await store.set("General Chemistry II", {
-    order: 0,
-    icon: "experiment",
-    color: "#ef4444",
-  });
+//   await store.set("General Chemistry II", {
+//     order: 0,
+//     icon: "experiment",
+//     color: "#ef4444",
+//   });
 
-  await store.set("Calculus II", {
-    order: 1,
-    icon: "function",
-    color: "#06b6d4",
-  });
+//   await store.set("Calculus II", {
+//     order: 1,
+//     icon: "function",
+//     color: "#06b6d4",
+//   });
 
-  await store.set("US Politics and Government", {
-    order: 2,
-    icon: "gavel",
-    color: "#10b981",
-  });
+//   await store.set("US Politics and Government", {
+//     order: 2,
+//     icon: "gavel",
+//     color: "#10b981",
+//   });
 
-  await store.set("Logical Reasoning", {
-    order: 3,
-    icon: "psychology",
-    color: "#eab308",
-  });
+//   await store.set("Logical Reasoning", {
+//     order: 3,
+//     icon: "psychology",
+//     color: "#eab308",
+//   });
 
-  const entries = ref([...store.entries<Space>()]);
+//   const entries = ref([...store.entries<Space>()]);
 
-  entries.value.sort(([, a], [, b]) => a.order - b.order);
+//   entries.value.sort(([, a], [, b]) => a.order - b.order);
 
-  spaces.value = Object.fromEntries(entries.value);
-}
+//   spaces.value = Object.fromEntries(entries.value);
+// }
 
 export function listSpaces() {
   console.log(spaces.value);
@@ -70,45 +69,9 @@ let activeFile;
 export async function readSpaceFile(
   kind: NoteKind,
   name: string,
-  path: string,
+  path: string
 ) {
-  const storage = useFirebaseStorage();
-  const db = useDatabase();
-  const user = useCurrentUser();
-
-  // watchEffect(() => {
-  //   console.log({ user: user.value });
-  // });
-
-  activeFile = dbRef(db, "spaces", user.value.uid, space, path);
-
-  // const doc = new Loro();
-  // const text: LoroText = doc.getText("text");
-  // text.insert(0, "Hello world!");
-
-  // activeFile.value = doc.exportSnapshot();
-  set(activeFile, doc.exportSnapshot());
-  // console.log(activeFile.value);
-  // console.log(doc.toJson()); // { "text": "Hello world!" }
-
-  // console.log("LMFAO,", await getBytes(activeFile));
   return "";
 }
 
-export async function syncSpaceFile(path: string, text: string) {
-  const storage = useFirebaseStorage();
-  const user = useCurrentUser();
-
-  return;
-
-  //   .then(async (url) => {
-  //     const content = await $fetch(url);
-
-  //     console.log({ content });
-  //   })
-  //   .catch(async () => {
-  //     console.log({ file });
-
-  //     await uploadBytes(file, new Blob());
-  //   });
-}
+export async function syncSpaceFile(path: string, text: string) {}
