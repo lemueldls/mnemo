59c2fc00726d73f708fb5c969e80a6fdc1551f50
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2024-09-13 14:31:38 -0400

    chore: cleanup

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 definePageMeta({ layout: "auth" });
-
-const href = ref();
-onMounted(() => {
-  href.value = window.location.href;
-});
 </script>
 
 <template>
   <m3-elevated-card class="gap-4 w-120">
     <h1 class="m3-title-large">Login</h1>
-
-    {{ href }}
 
     <md-filled-button href="/auth/github">
       Continue with GitHub
