459b659f2114584aa07e2bc59759b50310797de6
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2024-09-13 14:15:15 -0400

    test

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 definePageMeta({ layout: "auth" });
+
+const pathname = ref();
+onMounted(() => {
+  pathname.value = window.location.pathname;
+});
 </script>
 
 <template>
   <m3-elevated-card class="gap-4 w-120">
     <h1 class="m3-title-large">Login</h1>
+
+    {{ pathname }}
 
     <md-filled-button href="/auth/github">
       Continue with GitHub
