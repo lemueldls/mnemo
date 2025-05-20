cf9191ba6cab6a63e03473470f791401249dfae9
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-05-19 22:47:41 -0400

    fix: editor size

--- app/components/editor/index.vue
+++ app/components/editor/index.vue
@@ -229,15 +229,13 @@ const activeLineBackground = computed(() => {
 </script>
 
 <template>
-  <div class="size-full">
+  <div class="size-full overflow-x-auto overflow-y-scroll">
     <div ref="container" class="editor" />
   </div>
 </template>
 
 <style lang="scss">
 .editor {
-  @apply size-full overflow-x-auto overflow-y-scroll;
-
   .cm-editor {
     @apply m3-body-large h-full outline-none;
   }
