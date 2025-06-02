1072b6e1478a6b8f4c2a6c011b53b89bd0edde15
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-06-02 18:20:23 -0400

    fix: access origin

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -3,7 +3,8 @@ export default defineEventHandler(async (event) => {
     "Cross-Origin-Resource-Policy": "cross-origin",
     "Cross-Origin-Opener-Policy": "unsafe-none",
     "Cross-Origin-Embedder-Policy": "unsafe-none",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin":
+      "tauri://localhost https://tauri.localhost https://mnemo.nuxt.dev",
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Headers": "Content-Type",
   });
