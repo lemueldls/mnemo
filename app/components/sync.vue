9e3d5e8760aad785210c39ad82734f609fdf6bbc
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-06-02 19:48:30 -0400

    feat: quota progress

--- app/components/sync.vue
+++ app/components/sync.vue
@@ -16,17 +16,51 @@ async function logout() {
   const { error } = await auth.signOut();
   if (error) throw createError(error);
 }
+
+const quota = ref<number>();
+const usage = ref<number>();
+
+onMounted(async () => {
+  if (navigator.storage) {
+    const estimate = await navigator.storage.estimate();
+    quota.value = estimate.quota;
+    usage.value = estimate.usage;
+  }
+});
+
+const units = ["B", "KB", "MB", "GB", "TB", "PB"];
+function formatBytes(bytes: number) {
+  let i = 0;
+  while (bytes >= 1024 && i < units.length - 1) {
+    bytes /= 1024;
+    i++;
+  }
+
+  return `${bytes.toFixed(2)} ${units[i]}`;
+}
 </script>
 
 <template>
-  <h3 class="m3-display-small">Sync</h3>
+  <div class="flex flex-col gap-4">
+    <h3 class="m3-display-small">Sync</h3>
 
-  <div class="text-m3-error p-4">TODO</div>
+    <md-outlined-card v-if="usage && quota" class="flex flex-col gap-2 p-4">
+      <div class="m3-label-large flex justify-between">
+        <strong>Local Quota</strong>
 
-  <md-filled-tonal-button v-if="user" @click="logout">
-    Logout
-  </md-filled-tonal-button>
-  <md-filled-button v-else disabled @click="login">
-    Continue with GitHub
-  </md-filled-button>
+        <span>{{ formatBytes(usage) }} / {{ formatBytes(quota) }}</span>
+      </div>
+
+      <md-linear-progress :value="usage / quota" />
+    </md-outlined-card>
+
+    <span class="text-m3-error">TODO</span>
+
+    <md-filled-tonal-button v-if="user" @click="logout">
+      Logout
+    </md-filled-tonal-button>
+    <md-filled-button v-else disabled @click="login">
+      Continue with GitHub
+    </md-filled-button>
+  </div>
 </template>
