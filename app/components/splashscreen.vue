853a07b3fb2715e68caef963b5b6606e2f5e63a5
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-06-02 03:33:34 -0400

    feat: splashscreen

---
+++ app/components/splashscreen.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const progress = ref(0);
+const totalProgress = 3;
+
+const typstState = useTypst();
+typstState.then(() => progress.value++);
+
+onMounted(() => {
+  progress.value++;
+});
+onNuxtReady(async () => {
+  progress.value++;
+});
+
+const ready = computed(() => progress.value === totalProgress);
+</script>
+
+<template>
+  <m3-theme id="splashscreen" color="#16161d" dark :class="{ ready }">
+    <div class="w-sm flex flex-col gap-4">
+      <div class="m3-label-large flex justify-between">
+        <span class="font-bold">Loading...</span>
+
+        <span>{{ progress }} / {{ totalProgress }}</span>
+      </div>
+
+      <md-linear-progress class="h-2" :value="progress / totalProgress" />
+    </div>
+  </m3-theme>
+</template>
+
+<style>
+#splashscreen {
+  @apply text-m3-on-background bg-m3-background absolute inset-0 z-10 flex h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden opacity-100 transition-opacity;
+
+  background-color: #3f4178;
+  color: #c0c1ff;
+
+  &.ready {
+    @apply pointer-events-none select-none opacity-0;
+  }
+}
+</style>
