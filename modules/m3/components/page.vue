c3cb8b8a4ee719ef50a177019d14292dce53241b
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-06-08 02:33:52 -0400

    chore: update fonts

--- modules/m3/components/page.vue
+++ modules/m3/components/page.vue
@@ -7,7 +7,5 @@
 <style lang="scss">
 .m3-page {
   @apply text-m3-on-background bg-m3-background absolute inset-0 flex h-full w-full overflow-y-auto overflow-x-hidden;
-
-  font-family: var(--font-sans);
 }
 </style>
