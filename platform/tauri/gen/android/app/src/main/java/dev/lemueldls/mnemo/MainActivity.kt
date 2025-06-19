f76f67b3cf87dca6278cb415f50409481d8bc828
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-06-19 13:29:05 -0400

    fix: mobile layout

--- platform/tauri/gen/android/app/src/main/java/dev/lemueldls/mnemo/MainActivity.kt
+++ platform/tauri/gen/android/app/src/main/java/dev/lemueldls/mnemo/MainActivity.kt
@@ -1,17 +1,11 @@
 package dev.lemueldls.mnemo
 
-import android.graphics.Color
 import android.os.Bundle
-import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
-  }
-
-  override fun onWebViewCreate(webView: WebView) {
-    webView.setBackgroundColor(Color.TRANSPARENT)
   }
 }
