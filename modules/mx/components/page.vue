84b28ac9a81dbf5cd9a4f3549ece243b237feda8
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-07-02 22:21:13 -0400

    chore: update page ui

--- modules/mx/components/page.vue
+++ modules/mx/components/page.vue
@@ -6,6 +6,6 @@
 
 <style lang="scss">
 .page {
-  @apply text-on-background bg-background absolute inset-0 flex h-full w-full overflow-y-auto overflow-x-hidden;
+  @apply text-on-background bg-background absolute inset-0 flex h-screen w-full overflow-y-auto overflow-x-hidden;
 }
 </style>
