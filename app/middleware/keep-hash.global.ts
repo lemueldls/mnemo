36d060b92126480b08459972fa6550385895f515
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-07-07 07:57:09 -0400

    chore: cleanup

--- app/middleware/keep-hash.global.ts
+++ app/middleware/keep-hash.global.ts
@@ -2,8 +2,6 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const fromPath = from.fullPath.replace(/#.*$/, "");
   const toPath = to.fullPath.replace(/#.*$/, "");
 
-  if (from.hash) console.log({ fromPath, toPath });
-
   if (fromPath !== toPath && from.hash && !to.hash)
     return navigateTo({ ...to, hash: from.hash });
 });
