fc342eae702b499c5c70b3c3c699e94c7f9e0c54
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-07-09 18:08:54 -0400

    fix: auth

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,10 +1,12 @@
 export default defineEventHandler(async (event) => {
+  const url = getRequestURL(event);
+
   setHeaders(event, {
-    "Cross-Origin-Resource-Policy": "cross-origin",
-    "Cross-Origin-Opener-Policy": "unsafe-none",
-    "Cross-Origin-Embedder-Policy": "unsafe-none",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers": "Content-Type",
+    // "Cross-Origin-Resource-Policy": "cross-origin",
+    // "Cross-Origin-Opener-Policy": "unsafe-none",
+    // "Cross-Origin-Embedder-Policy": "unsafe-none",
+    "Access-Control-Allow-Origin": url.origin,
+    // "Access-Control-Allow-Credentials": "true",
+    // "Access-Control-Allow-Headers": "Content-Type",
   });
 });
