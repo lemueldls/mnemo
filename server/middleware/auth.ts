1580210a34bb6e52e40478b0b350dfce0057d1c3
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-07-10 21:18:35 -0400

    fix: platform header

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,15 +1,13 @@
 export default defineEventHandler(async (event) => {
+  if (event.method === "OPTIONS") return new Response(null, { status: 204 });
+
   const url = getRequestURL(event);
   const origin = getRequestHeader(event, "Origin");
 
   setHeaders(event, {
     "Access-Control-Allow-Origin": origin || url.origin,
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, platform",
     "Access-Control-Allow-Credentials": "true",
   });
-
-  if (event.method === "OPTIONS") {
-    return new Response(null, { status: 204 });
-  }
 });
