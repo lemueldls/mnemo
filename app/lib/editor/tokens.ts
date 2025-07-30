1a0df090a193dc04d6dae0bdffd71df6b86d9b02
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-07-30 09:47:59 -0400

    chore: update linting

--- app/lib/editor/tokens.ts
+++ app/lib/editor/tokens.ts
@@ -45,7 +45,7 @@ export const literalTokens = new ExternalTokenizer((input, stack) => {
         } while (isNum_(input.next));
       } else if (
         input.next == Dot ||
-        input.next > 0x7f ||
+        input.next > 0x7F ||
         /\w/.test(String.fromCharCode(input.next))
       ) {
         return;
