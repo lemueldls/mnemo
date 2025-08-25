13b5fb278dbddae96499456b4f7f7778fa5f073b
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-08-25 19:02:31 -0400

    chore: update tauri ci

--- platform/aur/generate-mnemo-bin.sh
+++ platform/aur/generate-mnemo-bin.sh
@@ -17,7 +17,7 @@ url="https://github.com/lemueldls/mnemo"
 license=('AGPL-3.0')
 depends=('cairo' 'desktop-file-utils' 'gdk-pixbuf2' 'glib2' 'gtk3' 'hicolor-icon-theme' 'libsoup' 'pango' 'webkit2gtk-4.1')
 options=('!strip' '!emptydirs')
-source_x86_64=("https://github.com/lemueldls/mnemo/releases/download/mnemo-v\$pkgver/Mnemo_\${pkgver}_amd64.deb")
+source_x86_64=("https://github.com/lemueldls/mnemo/releases/download/mnemo-v\$pkgver/mnemo-v\${pkgver}-amd64.deb")
 sha256sums_x86_64=('$sha256sum')
 package() {
     tar -xz -f data.tar.gz -C "\${pkgdir}"
