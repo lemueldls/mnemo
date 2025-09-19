eba681adefc924cca878ca62e3e1921d062ea1b6
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-09-19 14:58:44 -0400

    fix: catch updater error

--- app/composables/updater.ts
+++ app/composables/updater.ts
@@ -6,34 +6,38 @@ export async function checkForAppUpdates() {
   if (platform !== "windows" && platform !== "darwin" && platform !== "linux")
     return;
 
-  const update = await check();
-  if (!update) return;
+  try {
+    const update = await check();
+    if (!update) return;
 
-  console.log(
-    `found update ${update.version} from ${update.date} with notes ${update.body}`,
-  );
+    console.log(
+      `found update ${update.version} from ${update.date} with notes ${update.body}`,
+    );
 
-  let downloaded = 0;
-  let contentLength = 0;
+    let downloaded = 0;
+    let contentLength = 0;
 
-  // Alternatively, we could also call update.download() and update.install() separately
-  await update.downloadAndInstall((event) => {
-    switch (event.event) {
-      case "Started":
-        contentLength = event.data.contentLength!;
-        console.log(`started downloading ${event.data.contentLength} bytes`);
-        break;
-      case "Progress":
-        downloaded += event.data.chunkLength;
-        console.log(`downloaded ${downloaded} from ${contentLength}`);
-        break;
-      case "Finished":
-        console.log("download finished");
-        break;
-    }
-  });
+    // Alternatively, we could also call update.download() and update.install() separately
+    await update.downloadAndInstall((event) => {
+      switch (event.event) {
+        case "Started":
+          contentLength = event.data.contentLength!;
+          console.log(`started downloading ${event.data.contentLength} bytes`);
+          break;
+        case "Progress":
+          downloaded += event.data.chunkLength;
+          console.log(`downloaded ${downloaded} from ${contentLength}`);
+          break;
+        case "Finished":
+          console.log("download finished");
+          break;
+      }
+    });
 
-  console.log("update installed");
+    console.log("update installed");
 
-  await relaunch();
+    await relaunch();
+  } catch (error) {
+    console.error("Failed update:", error);
+  }
 }
