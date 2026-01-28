fad7649e52e4a562b575898a31e558c88871b9bc
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2026-01-28 11:32:00 -0500

    chore: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,5 @@
-import importPlugin from "eslint-plugin-import-x";
-import { globalIgnores } from "eslint/config";
+import oxlint from "eslint-plugin-oxlint";
 
-// @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt([
-  {
-    plugins: { import: importPlugin },
-    rules: {
-      "vue/html-self-closing": "off",
-      "vue/no-multiple-template-root": "off",
-      "vue/multi-word-component-names": "off",
-      "vue/max-attributes-per-line": "off",
-      "vue/singleline-html-element-content-newline": "off",
-      "vue/no-deprecated-slot-attribute": "off",
-
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/unified-signatures": "off",
-
-      "import/order": [
-        "warn",
-        {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
-            "object",
-            "type",
-          ],
-          "newlines-between": "always-and-inside-groups",
-          // "newlines-between-types": "always-and-inside-groups",
-          consolidateIslands: "inside-groups",
-          distinctGroup: true,
-          alphabetize: {
-            order: "asc",
-            orderImportKind: "asc",
-            caseInsensitive: true,
-          },
-          named: { enabled: true, types: "types-last" },
-          warnOnUnassignedImports: true,
-        },
-      ],
-    },
-  },
-  globalIgnores(["platform/*"]),
-]);
+export default withNuxt(...oxlint.configs["flat/recommended"]);
